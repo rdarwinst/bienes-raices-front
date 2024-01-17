8c66577d7b1a3e735ecb1d32cdfd4d5538828abd
Darwin Ramirez <rdarwinst@gmail.com>
2024-01-17 16:22:52 -0500

    Darkmode Añadido

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 const { src, dest, watch, series, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('autoprefixer');
-const postcss = require('gulp-postcss')
-const sourcemaps = require('gulp-sourcemaps')
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
 const cssnano = require('cssnano');
 const concat = require('gulp-concat');
 const terser = require('gulp-terser-js');
@@ -18,7 +18,7 @@ const paths = {
     scss: 'src/scss/**/*.scss',
     js: 'src/js/**/*.js',
     imagenes: 'src/img/**/*'
-}
+};
 
 function css() {
     return src(paths.scss)
@@ -32,12 +32,12 @@ function css() {
 
 function javascript() {
     return src(paths.js)
-      .pipe(sourcemaps.init())
-      .pipe(concat('bundle.js'))
-      .pipe(terser())
-      .pipe(sourcemaps.write('.'))
-      .pipe(rename({ suffix: '.min' }))
-      .pipe(dest('./build/js'))
+        .pipe(sourcemaps.init())
+        .pipe(concat('bundle.js'))
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(dest('./build/js'));
 }
 
 function imagenes() {
@@ -62,7 +62,7 @@ function versionAvif(done){
 
     src('src/img/**/*.{jpg,png}')
         .pipe(avif(opciones))
-        .pipe(dest('build/img'))
+        .pipe(dest('build/img'));
 
     done();
 }
