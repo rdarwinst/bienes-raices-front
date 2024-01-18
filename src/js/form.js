4e13a4dc0c85de788e2d5721ae3de048b29860f1
Darwin Ramirez <rdarwinst@gmail.com>
2024-01-18 16:52:43 -0500

    Formulario corregido

--- src/js/form.js
+++ src/js/form.js
@@ -1,32 +1,26 @@
-const nombrePagina = window.location.pathname;
-if (nombrePagina === '/contacto.html') {
-    iniciarVerificacion();
-}
+const datos = {
+    nombre: '',
+    email: '',
+    telefono: '',
+    mensaje: '',
+    opciones: '',
+    presupuesto: '',
+    contacto: '',
+    fecha: '',
+    hora: '',
+};
 
-function iniciarVerificacion() {
-    const datos = {
-        nombre: '',
-        email: '',
-        telefono: '',
-        mensaje: '',
-        opciones: '',
-        presupuesto: '',
-        contacto: '',
-        fecha: '',
-        hora: '',
-    };
+const nombre = document.querySelector('#nombre');
+const email = document.querySelector('#email');
+const telefono = document.querySelector('#telefono');
+const mensaje = document.querySelector('#mensaje');
+const opciones = document.querySelector('#opciones');
+const presupuesto = document.querySelector('#presupuesto');
+const fecha = document.querySelector('#fecha');
+const hora = document.querySelector('#hora');
+const formulario = document.querySelector('.formulario');
 
-    const formulario = document.querySelector('.formulario');
-
-    const nombre = document.querySelector('#nombre');
-    const email = document.querySelector('#email');
-    const telefono = document.querySelector('#telefono');
-    const mensaje = document.querySelector('#mensaje');
-    const opciones = document.querySelector('#opciones');
-    const presupuesto = document.querySelector('#presupuesto');
-    const fecha = document.querySelector('#fecha');
-    const hora = document.querySelector('#hora');
-
+try {
     nombre.addEventListener('input', leerDatos);
     email.addEventListener('input', leerDatos);
     telefono.addEventListener('input', leerDatos);
@@ -36,38 +30,40 @@ function iniciarVerificacion() {
     fecha.addEventListener('input', leerDatos);
     hora.addEventListener('input', leerDatos);
     formulario.addEventListener('submit', validarForm);
+} catch (error) {    
+    console.error(error);
+}
 
-    // Funciones 
+// Funciones 
 
-    function leerDatos(e) {
-        datos[e.target.id] = e.target.value;
+function leerDatos(e) {
+    datos[e.target.id] = e.target.value;
+}
+
+function validarForm(evt) {
+    evt.preventDefault();
+    const { nombre, email, telefono, mensaje, opciones, presupuesto, fecha, hora } = datos;
+
+    if (nombre === '' || email === '' || telefono === '' || mensaje === '' || opciones === '' || presupuesto === '' || fecha === '' || hora === '') {
+        mostrarAlerta('Debe llenar todos los campos.', true);
+        return;
+    }
+    mostrarAlerta('Formulario enviado correctamente.');
+    formulario.reset();
+}
+
+function mostrarAlerta(msj, error = null) {
+    const alerta = document.createElement('P');
+    alerta.textContent = msj;
+    if (error) {
+        alerta.classList.add('error');
+    } else {
+        alerta.classList.add('correcto');
     }
 
-    function validarForm(evt) {
-        evt.preventDefault();
-        const { nombre, email, telefono, mensaje, opciones, presupuesto, fecha, hora } = datos;
+    formulario.appendChild(alerta);
 
-        if (nombre === '' || email === '' || telefono === '' || mensaje === '' || opciones === '' || presupuesto === '' || fecha === '' || hora === '') {
-            mostrarAlerta('Debe llenar todos los campos.', true);
-            return;
-        }
-        mostrarAlerta('Formulario enviado correctamente.');
-        formulario.reset();
-    }
-
-    function mostrarAlerta(msj, error = null) {
-        const alerta = document.createElement('P');
-        alerta.textContent = msj;
-        if (error) {
-            alerta.classList.add('error');
-        } else {
-            alerta.classList.add('correcto');
-        }
-
-        formulario.appendChild(alerta);
-
-        setTimeout(() => {
-            alerta.remove();
-        }, 3000);
-    }
+    setTimeout(() => {
+        alerta.remove();
+    }, 3000);
 }
