2d08e955a5878aecc11984842b7768f68b9708b0
Darwin Ramirez <rdarwinst@gmail.com>
2024-01-18 15:50:55 -0500

    form.js: modificado

--- src/js/form.js
+++ src/js/form.js
@@ -1,30 +1,34 @@
-const datos = {
-    nombre: '',
-    email: '',
-    telefono: '',
-    mensaje: '',
-    opciones: '',
-    presupuesto: '',
-    contacto: '',
-    fecha: '',
-    hora: '',
-};
-
-const formulario = document.querySelector('.formulario');
-
-const nombre = document.querySelector('#nombre');
-const email = document.querySelector('#email');
-const telefono = document.querySelector('#telefono');
-const mensaje = document.querySelector('#mensaje');
-const opciones = document.querySelector('#opciones');
-const presupuesto = document.querySelector('#presupuesto');
-const fecha = document.querySelector('#fecha');
-const hora = document.querySelector('#hora');
-
-
 const url = window.location.href;
 
 if (url.includes('contacto.html')) {
+    form();
+}
+
+
+function form() {
+    const datos = {
+        nombre: '',
+        email: '',
+        telefono: '',
+        mensaje: '',
+        opciones: '',
+        presupuesto: '',
+        contacto: '',
+        fecha: '',
+        hora: '',
+    };
+
+    const formulario = document.querySelector('.formulario');
+
+    const nombre = document.querySelector('#nombre');
+    const email = document.querySelector('#email');
+    const telefono = document.querySelector('#telefono');
+    const mensaje = document.querySelector('#mensaje');
+    const opciones = document.querySelector('#opciones');
+    const presupuesto = document.querySelector('#presupuesto');
+    const fecha = document.querySelector('#fecha');
+    const hora = document.querySelector('#hora');
+
     nombre.addEventListener('input', leerDatos);
     email.addEventListener('input', leerDatos);
     telefono.addEventListener('input', leerDatos);
@@ -33,40 +37,39 @@ if (url.includes('contacto.html')) {
     presupuesto.addEventListener('input', leerDatos);
     fecha.addEventListener('input', leerDatos);
     hora.addEventListener('input', leerDatos);
-
     formulario.addEventListener('submit', validarForm);
-}
 
-// Funciones 
+    // Funciones 
 
-function leerDatos(e) {
-    datos[e.target.id] = e.target.value;
-}
-
-function validarForm(evt) {
-    evt.preventDefault();
-    const { nombre, email, telefono, mensaje, opciones, presupuesto, fecha, hora } = datos;
-
-    if (nombre === '' || email === '' || telefono === '' || mensaje === '' || opciones === '' || presupuesto === '' || fecha === '' || hora === '') {
-        mostrarAlerta('Debe llenar todos los campos.', true);
-        return;
-    }
-    mostrarAlerta('Formulario enviado correctamente.');
-    formulario.reset();
-}
-
-function mostrarAlerta(msj, error = null) {
-    const alerta = document.createElement('P');
-    alerta.textContent = msj;
-    if (error) {
-        alerta.classList.add('error');
-    } else {
-        alerta.classList.add('correcto');
+    function leerDatos(e) {
+        datos[e.target.id] = e.target.value;
     }
 
-    formulario.appendChild(alerta);
+    function validarForm(evt) {
+        evt.preventDefault();
+        const { nombre, email, telefono, mensaje, opciones, presupuesto, fecha, hora } = datos;
 
-    setTimeout(() => {
-        alerta.remove();
-    }, 3000);
+        if (nombre === '' || email === '' || telefono === '' || mensaje === '' || opciones === '' || presupuesto === '' || fecha === '' || hora === '') {
+            mostrarAlerta('Debe llenar todos los campos.', true);
+            return;
+        }
+        mostrarAlerta('Formulario enviado correctamente.');
+        formulario.reset();
+    }
+
+    function mostrarAlerta(msj, error = null) {
+        const alerta = document.createElement('P');
+        alerta.textContent = msj;
+        if (error) {
+            alerta.classList.add('error');
+        } else {
+            alerta.classList.add('correcto');
+        }
+
+        formulario.appendChild(alerta);
+
+        setTimeout(() => {
+            alerta.remove();
+        }, 3000);
+    }
 }
